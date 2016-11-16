1bf15383ba9a584dc36fe633c5033c52d56c58f2
wangw571 <wang_w571@hotmail.com>
2016-11-16 11:14:32 -0500

    Creating Main Form

---
+++ Attendance-Scanning/Attendance-Scanning/Main.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Attendance_Scanning
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Box_StudentIndex = new System.Windows.Forms.TextBox();
+            this.Label_StudentIndex = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.B_LoadFile = new System.Windows.Forms.ToolStripButton();
+            this.B_DataSave = new System.Windows.Forms.ToolStripButton();
+            this.FileOpener = new System.Windows.Forms.OpenFileDialog();
+            this.FileSaver = new System.Windows.Forms.SaveFileDialog();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.toolStrip1);
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1073, 172);
+            this.panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.Box_StudentIndex, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Label_StudentIndex, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 39);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(226, 73);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // Box_StudentIndex
+            // 
+            this.Box_StudentIndex.Location = new System.Drawing.Point(3, 39);
+            this.Box_StudentIndex.Name = "Box_StudentIndex";
+            this.Box_StudentIndex.Size = new System.Drawing.Size(208, 28);
+            this.Box_StudentIndex.TabIndex = 0;
+            // 
+            // Label_StudentIndex
+            // 
+            this.Label_StudentIndex.AutoSize = true;
+            this.Label_StudentIndex.Location = new System.Drawing.Point(3, 0);
+            this.Label_StudentIndex.Name = "Label_StudentIndex";
+            this.Label_StudentIndex.Size = new System.Drawing.Size(143, 18);
+            this.Label_StudentIndex.TabIndex = 1;
+            this.Label_StudentIndex.Text = "Student Index: ";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.B_LoadFile,
+            this.toolStripSeparator1,
+            this.B_DataSave});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.toolStrip1.Size = new System.Drawing.Size(1073, 31);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // B_LoadFile
+            // 
+            this.B_LoadFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.B_LoadFile.Image = ((System.Drawing.Image)(resources.GetObject("B_LoadFile.Image")));
+            this.B_LoadFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.B_LoadFile.Name = "B_LoadFile";
+            this.B_LoadFile.Size = new System.Drawing.Size(175, 28);
+            this.B_LoadFile.Text = "Load Student Data";
+            // 
+            // B_DataSave
+            // 
+            this.B_DataSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.B_DataSave.Image = ((System.Drawing.Image)(resources.GetObject("B_DataSave.Image")));
+            this.B_DataSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.B_DataSave.Name = "B_DataSave";
+            this.B_DataSave.Size = new System.Drawing.Size(99, 28);
+            this.B_DataSave.Text = "Save Data";
+            // 
+            // FileOpener
+            // 
+            this.FileOpener.Filter = "Comma Split Value File|*.csv";
+            this.FileOpener.Title = "Load Student data";
+            // 
+            // FileSaver
+            // 
+            this.FileSaver.Filter = "Comma Split Value File|*.csv";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 31);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1073, 768);
+            this.Controls.Add(this.panel1);
+            this.Name = "Main";
+            this.Text = "Student Attendance Manager";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Label_StudentIndex;
+        private System.Windows.Forms.TextBox Box_StudentIndex;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton B_LoadFile;
+        private System.Windows.Forms.ToolStripButton B_DataSave;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.OpenFileDialog FileOpener;
+        private System.Windows.Forms.SaveFileDialog FileSaver;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
+
--- Attendance-Scanning/Attendance-Scanning/Main.cs
+++ Attendance-Scanning/Attendance-Scanning/Main.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Attendance_Scanning
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
