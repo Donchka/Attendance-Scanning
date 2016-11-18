35143e518e3905030d3d19388932171f5aab402e
wangw571 <wang_w571@hotmail.com>
2016-11-18 12:34:44 -0500

    Nope. Re merge again

--- Attendance-Scanning/Attendance-Scanning/EmailEditorDialog.Designer.cs
+++ Attendance-Scanning/Attendance-Scanning/EmailEditorDialog.Designer.cs
@@ -40,16 +40,17 @@
             this.ClearAllButton = new System.Windows.Forms.Button();
             this.SelectionPanel = new System.Windows.Forms.TableLayoutPanel();
             this.TextPanel = new System.Windows.Forms.Panel();
+            this.ButtonPanel = new System.Windows.Forms.Panel();
             this.SelectionPanel.SuspendLayout();
             this.TextPanel.SuspendLayout();
+            this.ButtonPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // Save
             // 
-            this.Save.Location = new System.Drawing.Point(387, 425);
-            this.Save.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Save.Location = new System.Drawing.Point(581, 12);
             this.Save.Name = "Save";
-            this.Save.Size = new System.Drawing.Size(91, 27);
+            this.Save.Size = new System.Drawing.Size(136, 37);
             this.Save.TabIndex = 0;
             this.Save.Text = "Save Format";
             this.Save.UseVisualStyleBackColor = true;
@@ -57,10 +58,9 @@
             // 
             // Cancel
             // 
-            this.Cancel.Location = new System.Drawing.Point(8, 427);
-            this.Cancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Cancel.Location = new System.Drawing.Point(6, 9);
             this.Cancel.Name = "Cancel";
-            this.Cancel.Size = new System.Drawing.Size(83, 30);
+            this.Cancel.Size = new System.Drawing.Size(124, 42);
             this.Cancel.TabIndex = 1;
             this.Cancel.Text = "Cancel";
             this.Cancel.UseVisualStyleBackColor = true;
@@ -68,28 +68,29 @@
             // 正文
             // 
             this.正文.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.正文.Location = new System.Drawing.Point(0, 38);
+            this.正文.Location = new System.Drawing.Point(0, -179);
+            this.正文.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.正文.Multiline = true;
             this.正文.Name = "正文";
             this.正文.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.正文.Size = new System.Drawing.Size(715, 407);
+            this.正文.Size = new System.Drawing.Size(710, 562);
             this.正文.TabIndex = 2;
             // 
             // TitleTextBox
             // 
             this.TitleTextBox.Dock = System.Windows.Forms.DockStyle.Top;
             this.TitleTextBox.Location = new System.Drawing.Point(0, 0);
+            this.TitleTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TitleTextBox.Name = "TitleTextBox";
-            this.TitleTextBox.Size = new System.Drawing.Size(715, 28);
+            this.TitleTextBox.Size = new System.Drawing.Size(710, 28);
             this.TitleTextBox.TabIndex = 3;
             // 
             // StudentNameLabel
             // 
             this.StudentNameLabel.AutoSize = true;
             this.StudentNameLabel.Location = new System.Drawing.Point(3, 0);
-            this.StudentNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.StudentNameLabel.Name = "StudentNameLabel";
-            this.StudentNameLabel.Size = new System.Drawing.Size(206, 13);
+            this.StudentNameLabel.Size = new System.Drawing.Size(341, 18);
             this.StudentNameLabel.TabIndex = 4;
             this.StudentNameLabel.TabStop = true;
             this.StudentNameLabel.Text = "Use <StudentName> to add student name";
@@ -98,10 +99,9 @@
             // StudentLateTimeLabel
             // 
             this.StudentLateTimeLabel.AutoSize = true;
-            this.StudentLateTimeLabel.Location = new System.Drawing.Point(3, 33);
-            this.StudentLateTimeLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.StudentLateTimeLabel.Location = new System.Drawing.Point(3, 45);
             this.StudentLateTimeLabel.Name = "StudentLateTimeLabel";
-            this.StudentLateTimeLabel.Size = new System.Drawing.Size(259, 13);
+            this.StudentLateTimeLabel.Size = new System.Drawing.Size(467, 18);
             this.StudentLateTimeLabel.TabIndex = 5;
             this.StudentLateTimeLabel.TabStop = true;
             this.StudentLateTimeLabel.Text = "Use <StudentLateTime> to add the time student lated";
@@ -110,10 +110,9 @@
             // ClassNameLabel
             // 
             this.ClassNameLabel.AutoSize = true;
-            this.ClassNameLabel.Location = new System.Drawing.Point(3, 69);
-            this.ClassNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.ClassNameLabel.Location = new System.Drawing.Point(3, 95);
             this.ClassNameLabel.Name = "ClassNameLabel";
-            this.ClassNameLabel.Size = new System.Drawing.Size(206, 13);
+            this.ClassNameLabel.Size = new System.Drawing.Size(350, 18);
             this.ClassNameLabel.TabIndex = 6;
             this.ClassNameLabel.TabStop = true;
             this.ClassNameLabel.Text = "Use <ClassName> to add your class name";
@@ -122,10 +121,9 @@
             // TeacherNameLabel
             // 
             this.TeacherNameLabel.AutoSize = true;
-            this.TeacherNameLabel.Location = new System.Drawing.Point(3, 105);
-            this.TeacherNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.TeacherNameLabel.Location = new System.Drawing.Point(3, 145);
             this.TeacherNameLabel.Name = "TeacherNameLabel";
-            this.TeacherNameLabel.Size = new System.Drawing.Size(194, 13);
+            this.TeacherNameLabel.Size = new System.Drawing.Size(314, 18);
             this.TeacherNameLabel.TabIndex = 7;
             this.TeacherNameLabel.TabStop = true;
             this.TeacherNameLabel.Text = "Use <TeacherName> to add your name";
@@ -133,10 +131,9 @@
             // 
             // ExapmleApplier
             // 
-            this.ExapmleApplier.Location = new System.Drawing.Point(115, 427);
-            this.ExapmleApplier.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ExapmleApplier.Location = new System.Drawing.Point(165, 15);
             this.ExapmleApplier.Name = "ExapmleApplier";
-            this.ExapmleApplier.Size = new System.Drawing.Size(101, 22);
+            this.ExapmleApplier.Size = new System.Drawing.Size(152, 30);
             this.ExapmleApplier.TabIndex = 8;
             this.ExapmleApplier.Text = "Apply Example";
             this.ExapmleApplier.UseVisualStyleBackColor = true;
@@ -144,10 +141,9 @@
             // 
             // ClearAllButton
             // 
-            this.ClearAllButton.Location = new System.Drawing.Point(253, 427);
-            this.ClearAllButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ClearAllButton.Location = new System.Drawing.Point(390, 15);
             this.ClearAllButton.Name = "ClearAllButton";
-            this.ClearAllButton.Size = new System.Drawing.Size(101, 22);
+            this.ClearAllButton.Size = new System.Drawing.Size(152, 30);
             this.ClearAllButton.TabIndex = 9;
             this.ClearAllButton.Text = "Clear All";
             this.ClearAllButton.UseVisualStyleBackColor = true;
@@ -163,42 +159,53 @@
             this.SelectionPanel.Controls.Add(this.TeacherNameLabel, 0, 3);
             this.SelectionPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.SelectionPanel.Location = new System.Drawing.Point(0, 0);
+            this.SelectionPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SelectionPanel.Name = "SelectionPanel";
             this.SelectionPanel.RowCount = 4;
             this.SelectionPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.22222F));
             this.SelectionPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.77778F));
-            this.SelectionPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
-            this.SelectionPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
-            this.SelectionPanel.Size = new System.Drawing.Size(729, 131);
+            this.SelectionPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.SelectionPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.SelectionPanel.Size = new System.Drawing.Size(729, 181);
             this.SelectionPanel.TabIndex = 10;
             // 
             // TextPanel
             // 
             this.TextPanel.Controls.Add(this.TitleTextBox);
             this.TextPanel.Controls.Add(this.正文);
-            this.TextPanel.Location = new System.Drawing.Point(12, 137);
+            this.TextPanel.Location = new System.Drawing.Point(6, 189);
+            this.TextPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TextPanel.Name = "TextPanel";
-            this.TextPanel.Size = new System.Drawing.Size(715, 445);
+            this.TextPanel.Size = new System.Drawing.Size(710, 383);
             this.TextPanel.TabIndex = 11;
+            // 
+            // ButtonPanel
+            // 
+            this.ButtonPanel.Controls.Add(this.Cancel);
+            this.ButtonPanel.Controls.Add(this.ExapmleApplier);
+            this.ButtonPanel.Controls.Add(this.Save);
+            this.ButtonPanel.Controls.Add(this.ClearAllButton);
+            this.ButtonPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ButtonPanel.Location = new System.Drawing.Point(0, 579);
+            this.ButtonPanel.Name = "ButtonPanel";
+            this.ButtonPanel.Size = new System.Drawing.Size(729, 54);
+            this.ButtonPanel.TabIndex = 12;
             // 
             // EmailEditorDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(486, 457);
+            this.ClientSize = new System.Drawing.Size(729, 633);
+            this.Controls.Add(this.ButtonPanel);
             this.Controls.Add(this.TextPanel);
             this.Controls.Add(this.SelectionPanel);
-            this.Controls.Add(this.ClearAllButton);
-            this.Controls.Add(this.ExapmleApplier);
-            this.Controls.Add(this.Cancel);
-            this.Controls.Add(this.Save);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "EmailEditorDialog";
             this.Text = "Email Format Editor";
             this.SelectionPanel.ResumeLayout(false);
             this.SelectionPanel.PerformLayout();
             this.TextPanel.ResumeLayout(false);
             this.TextPanel.PerformLayout();
+            this.ButtonPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -217,5 +224,6 @@
         private System.Windows.Forms.Button ClearAllButton;
         private System.Windows.Forms.TableLayoutPanel SelectionPanel;
         private System.Windows.Forms.Panel TextPanel;
+        private System.Windows.Forms.Panel ButtonPanel;
     }
 }
