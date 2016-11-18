6f78b62188904e1d6318c5e03456f09fb7eb679e
IcariaVoltage <kevin4051@gmail.com>
2016-11-18 12:47:37 -0500

    This is the TimeKeeper

--- TimeKeeper.cs
+++ TimeKeeper.cs
@@ -9,26 +9,37 @@ using System.Timers;
 namespace Attendance_Scanning
 {
     //This class will take the students time and say if they are on time or not
-    public class TimeKeeper 
+    class TimeKeeper 
     { 
+      
        //this will call the functions below
        //call TimeBracket
     }
 
-    private enum TimeBracket
+    public class TimeBracket
     {
         //this is the time limit a student has before being marked absnt
         //will use TimeLog and SwipeIn 
     }
 
-    private class TimeLog
+    public class TimeLog
     {
         //This logs the students into the system when they scan their cards
+        //var TimeIN = StudentID + " arrived at " + mm/dd/yy/hh:mm;
+
+        //var TimeLATE{
+            
+
+        //}
+
     }
 
-    private enum SwipeIn
+    public enum SwipeIn
     {
         //extends to the scanning feature Charlie is working on
+        //TimeIN;
+        //TimeLATE;
+        //TimeABSENT;
     }
 
 }
